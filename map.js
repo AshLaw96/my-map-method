624b4712ca8923b98a03326cb376bf36a79e1b87
Ashley Lawrence <ash.l.96@outlook.com>
2024-10-28 15:18:21 +0000

    Add map method content to the map.js file

--- map.js
+++ map.js
@@ -6,15 +6,31 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
 
+const multiplyByTwo = function(num) {
+  return num * 2;
+}
+
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const simplified = nums.map(function(num) {return num * 2});
+console.log(simplified);
 
-// Simplfied w/ map() + arrow function
+// Simplified w/ map() + arrow function
 
+const simpleMap = nums.map(n => n * 2);
+console.log(simpleMap);
 
 // With objects:
 const students = [
@@ -37,3 +53,18 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const objStudents = students.map(student => ({
+  id: student.id,
+  name: student.name
+}));
+console.log(objStudents);
+
+const addAge = students.map(student => ({
+  ...student,
+  age: 22 + student.id * 2
+}));
+console.log(addAge);
